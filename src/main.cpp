0bf5f9da4aa351ac946f929e6cd5457aad1e8579
ansh-codr <anshloverao@gmail.com>
2026-02-21 22:32:51 +0530

    initialize Serial and configure RGB output pins

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,13 @@ int GREEN_pin = 10;
 int BLUE_pin = 11;
 
 void setup() {
+    Serial.begin(9600);
+
+    pinMode(RED_pin, OUTPUT);
+    pinMode(GREEN_pin, OUTPUT);
+    pinMode(BLUE_pin, OUTPUT);
+
+    Serial.println("RGB LED Initialized!");
 }
 
 void loop() {
